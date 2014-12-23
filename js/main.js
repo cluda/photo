1474621b1c1de393f87d23645894050e4c559790
cluda <asgeir@cluda.no>
2014-12-23 22:38:31 +0100

    Only the index page has changing background picture.

--- js/main.js
+++ js/main.js
@@ -5,14 +5,21 @@ $.get("{{ site.baseurl }}/background_pictures.csv", function (data) {
   var sliderImages = data.split(',');
   $('body').css('backgroundImage', 'url(' + sliderImages[0] + ')');
 
-  var i = 0;
-  setInterval(function () {
-    $('body').css('backgroundImage', 'url(' + sliderImages[i] + ')');
-    i = i + 1;
-    if (i >= sliderImages.length) {
-      i = 0;
+  var url = document.URL;
+  var filename = url.substring(url.lastIndexOf('/')+1);
+  console.log( filename );
+
+    if ( filename === "" || filename === "index.html" ) {
+        var i = 0;
+        setInterval(function () {
+            $('body').css('backgroundImage', 'url(' + sliderImages[i] + ')');
+            i = i + 1;
+            if (i >= sliderImages.length) {
+                i = 0;
+            }
+        }, 4000);
     }
-  }, 4000);
+
 });
 
 $('.js-accordion-trigger').bind('click', function (e) {
